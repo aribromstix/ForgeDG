d99e298dbc37a7334d41f0e5dbb44ecf9e7fc53a
Adam Kaczorowski <adam.kaczorowski84@gmail.com>
2025-06-16 11:36:07 +0200

    Remove duplicate Stripe init; keep single Stripe instance

--- sales/webhook.js
+++ sales/webhook.js
@@ -1,6 +1,7 @@
+// sales/webhook.js
 const Stripe = require('stripe');
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_dummy');// sales/webhook.js
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_dummy');
+
 module.exports.handler = async (event) => {
   // TODO: obsłuż checkout.session.completed
   return { statusCode: 200, body: 'OK' };
